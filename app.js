a8cfe47977f0651a91ee2c57304399e4fd85636d
nagireddyakhilredddy <S545143@NWMISSOURI.EDU>
2021-10-27 14:52:38 -0500

    new endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydataRouter = require('./routes/index');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/',indexRouter);
 app.use('/', mydataRouter);
 app.use('/users', usersRouter);
 
