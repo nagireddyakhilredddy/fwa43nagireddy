76f4a37e1840b20c08b895743325055806baa3a0
nagireddyakhilredddy <S545143@NWMISSOURI.EDU>
2021-10-27 15:28:38 -0500

    final commit

--- routes/computation.js
+++ routes/computation.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var value=Math.round(Math.random()*100);
 
-var s=Math.hypot()  ;
-var t=Math.ceil()  
-var h=Math.clz32()
+var s=Math.hypot(value)  ;
+var t=Math.ceil(value)  
+var h=Math.clz32(value)
  
 
 /* GET home page. */
